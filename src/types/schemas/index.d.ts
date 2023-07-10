1e880500706363e242dafa82eb3e9de029d3136a
zoton2 <zoton2@gmail.com>
2023-07-10 12:53:07 +0100

    Bump deps, @alvancamp/test-nodecg-types > @nodecg/types

--- src/types/schemas/index.d.ts
+++ src/types/schemas/index.d.ts
@@ -1,4 +1,9 @@
+/* eslint-disable */
+// @ts-ignore
 export * from './configschema';
+// @ts-ignore
 export * from './cycles';
+// @ts-ignore
 export * from './disabled';
+// @ts-ignore
 export * from './toggle';
