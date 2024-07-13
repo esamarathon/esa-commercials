4bf63c270fdf7a3ae30a6d9d3db9e03c1124a956
zoton2 <zoton2@gmail.com>
2024-07-13 10:54:50 +0100

    Lots of improvements from template, bump deps

--- src/extension/util/replicants.ts
+++ src/extension/util/replicants.ts
@@ -5,12 +5,21 @@ import type NodeCG from '@nodecg/types';
 import { TwitchChannelInfo } from 'speedcontrol-util/types/schemas';
 import { get as nodecg } from './nodecg';
 
+type Opts<T> = NodeCG.Replicant.Options<T>;
+// Wrapper for replicants that have a default (based on schema).
+function hasDefault<T>(name: string, namespaceOrOpts?: string | Opts<T> | undefined, opts?: Opts<T> | undefined) {
+  return nodecg().Replicant<T>(name, namespaceOrOpts, opts) as unknown as NodeCG.ServerReplicantWithSchemaDefault<T>;
+}
+// Wrapper for replicants that don't have a default (based on schema).
+function hasNoDefault<T>(name: string, namespaceOrOpts?: string | Opts<T> | undefined, opts?: Opts<T> | undefined) {
+  return nodecg().Replicant<T>(name, namespaceOrOpts, opts) as unknown as NodeCG.ServerReplicant<T>;
+}
+
 /**
  * This is where you can declare all your replicant to import easily into other files,
  * and to make sure they have any correct settings on startup.
  */
-
-export const cycles = nodecg().Replicant<Cycles>('cycles') as unknown as NodeCG.ServerReplicantWithSchemaDefault<Cycles>;
-export const disabled = nodecg().Replicant<Disabled>('disabled') as unknown as NodeCG.ServerReplicantWithSchemaDefault<Disabled>;
-export const toggle = nodecg().Replicant<Toggle>('toggle') as unknown as NodeCG.ServerReplicantWithSchemaDefault<Toggle>;
-export const twitchChannelInfo = nodecg().Replicant<TwitchChannelInfo>('twitchChannelInfo', 'nodecg-speedcontrol') as unknown as NodeCG.ServerReplicant<TwitchChannelInfo>;
+export const cycles = hasDefault<Cycles>('cycles');
+export const disabled = hasDefault<Disabled>('disabled');
+export const toggle = hasDefault<Toggle>('toggle');
+export const twitchChannelInfo = hasNoDefault<TwitchChannelInfo>('twitchChannelInfo', 'nodecg-speedcontrol');
